bc3b1e3847a09de4fd2acd2bb680044126f6bbbe
matejbolko <matej.bolko@gmail.com>
2018-05-03 21:15:45 +0200

    unmatched function and disabled mouse click
    unmatched cards function and logic around comparing two cards and finding  which symbol has the card

--- js/app.js
+++ js/app.js
@@ -22,7 +22,7 @@ function CreateCards () {
   // create 12li classes named "card"
   for (let x = 0; x < 12; x++) {
     let li = document.createElement('li')
-    li.className = 'card show match'
+    li.className = 'card'
     li.id = "cardid"+x;
 
     var i = document.createElement('i')
@@ -34,17 +34,61 @@ function CreateCards () {
   }
 }
 
+function unmatchedCards (el1,el2) {
+  let tmp = document.getElementsByClassName('container')
+  tmp[0].classList.add('disable')
+  console.log("el1: "+el1+", el2: "+el2);
+  window.setTimeout(function() {
+    el1.classList.remove('show', 'open')
+    el2.classList.remove('show', 'open')
+    counter = 0;
+    tmp[0].classList.remove('disable')
+  },1000);
+}
+
 function rotateCard (e) {
   let id = e.target.id;
   let element = e.target;
   if (element.classList.contains('fa') === true) {
-    console.log("znotraj zanke")
+    // if clicked on symbol - get parentEl (card) ID
     id = e.target.parentElement.id;
     element = e.target.parentElement;
   }
-  console.log("to je ID: " +id)
-  console.log("to je el: " +element)
-  element.classList.remove("show")
+  // console.log("this is card ID: " +id)
+
+  // on clikc - open card (if it is not already opened)
+  if (openedCards.includes(id) === false) {
+    openedCards.push(id);
+    element.classList.add('show')
+    element.classList.add('open')
+    counter++;
+  }
+  else {
+  }
+
+  // if 2 cards are opened - check if matched or missmatched
+  if (counter == 2) {
+    // read 2 cards == 2 UL classes
+    let el1 = openedCards.pop();
+    let el2 = openedCards.pop();
+    el1 = document.getElementById(el1)
+    console.log("el1: "+el1);
+    el2 = document.getElementById(el2)
+    // read and compare 2 child LI classes
+    let elch1 = el1.getElementsByClassName('fa')
+    let elch2 = el2.getElementsByClassName('fa')
+    elch1 =  elch1[0].classList.item(1);
+    elch2 =  elch2[0].classList.item(1);
+    console.log("first: "+elch1);
+    console.log("second: "+elch2);
+    if (elch1 == elch2) {
+      console.log("zadel si, TO DO")
+    }
+    else {
+      unmatchedCards(el1, el2);
+    }
+      //match2Cards()
+  }
 }
 
 function restartGame() {
@@ -59,6 +103,8 @@ var memoryCards = ['fa-diamond', 'fa-diamond', 'fa-paper-plane-o', 'fa-paper-pla
 shuffle(memoryCards)
 // console.log("shuffledCards: "+shuffledCards);
 CreateCards()
+var counter = 0;
+var openedCards = [];
 document.querySelector(".deck").addEventListener("click", rotateCard)
 document.querySelector(".restart").addEventListener("click",restartGame)
 
